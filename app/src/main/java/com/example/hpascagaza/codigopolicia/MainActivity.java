df404770e0c219ab3ad0e6ee3ce8a9c623e350ae
hpascagaza <holmanpascagaza@gmail.com>
2020-01-03 08:38:05 -0500

    Modificaciones inicio

--- app/src/main/java/com/example/hpascagaza/codigopolicia/MainActivity.java
+++ app/src/main/java/com/example/hpascagaza/codigopolicia/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.hpascagaza.codigopolicia;
 
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.FragmentManager;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,7 +13,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener
 {
 
     @Override
@@ -71,56 +72,37 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item)
     {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment;
 
         if (id == R.id.nav_home)
         {
-            //fragmentManager.add(R.id.contenedor, fragment);
+            fragment = new HomeFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.mainFrame, fragment);
+            ft.commit();
         }
-        else if (id == R.id.nav_code_police)
+        else if (id == R.id.nav_code1)
+        {
+            fragment = new Code1Fragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.mainFrame, fragment);
+            ft.commit();
+        }
+        else if (id == R.id.nav_code2)
         {
 
         }
-        else if (id == R.id.nav_alcoholDrogas)
+        else if (id == R.id.nav_code3)
         {
 
-        }
-        else if (id == R.id.nav_animalesMascotas)
+        } else if (id == R.id.nav_code4)
         {
 
-        }
-        else if (id == R.id.nav_porteArmas)
-        {
-
-        }
-        else if (id == R.id.nav_peatonesBicicletas)
-        {
-
-        }
-        else if (id == R.id.nav_transportePublico)
-        {
-
-        }
-        else if (id == R.id.nav_espacioPublico)
-        {
-
-        }
-        else if (id == R.id.nav_actividadComercial)
-        {
-
-        }
-        else if (id == R.id.nav_protestaSocial)
-        {
-
-        }
-        else if (id == R.id.nav_convivenciaEntreVecinos)
+        } else if (id == R.id.nav_code5)
         {
 
         }
